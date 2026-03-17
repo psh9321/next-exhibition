5fea8fb3dfeaccbaaa13a745721abaa9d3372516
psh9321 <parksuhyun9321@gmail.com>
2026-03-18 04:46:33 +0900

    portal 수정
     - if 문 예외처리 제거 (에러남)

--- src/shared/ui/Portal/index.tsx
+++ src/shared/ui/Portal/index.tsx
@@ -2,14 +2,11 @@
 
 import ReactDOM from "react-dom";
 
-import { useEffect, useState } from "react";
-
 export const Portal = ({ children } : LAYOUT_CHILD) => {
 
     const elements = document.getElementById("portal-root");
 
-    if(!elements) return alert("portal-root undefined");
-    if(elements.hasChildNodes()) return
+    if(elements?.hasChildNodes()) return
 
     return ReactDOM.createPortal( children, elements! );
 }
