3e95efa68585b6961529603e3f31b33957a255e6
psh9321 <parksuhyun9321@gmail.com>
2026-03-18 04:43:56 +0900

    portal 수정
     - portal-root 가 없을시 예외처리
     - portal-root 안에 자식요소가 있을시 리턴 처리

--- src/shared/ui/Portal/index.tsx
+++ src/shared/ui/Portal/index.tsx
@@ -6,18 +6,10 @@ import { useEffect, useState } from "react";
 
 export const Portal = ({ children } : LAYOUT_CHILD) => {
 
-    const [ isMount, SetIsMound ] = useState<boolean>(false);
-
-    // useEffect(() => {
-    //     SetIsMound(true);
-    // },[]);
-
-
-    // if(!isMount) return <>{children}</>
-
-    // if(!document.getElementById("portal-root")) return <>{children}</>
-
     const elements = document.getElementById("portal-root");
+
+    if(!elements) return alert("portal-root undefined");
+    if(elements.hasChildNodes()) return
 
     return ReactDOM.createPortal( children, elements! );
 }
