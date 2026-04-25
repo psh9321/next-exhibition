127fc8f2dbfa5180d085b43474f181191ab75863
psh9321 <parksuhyun9321@gmail.com>
2026-04-26 03:18:22 +0900

    ecosystem.config.js port 번호 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,11 @@ module.exports = {
         {
             name : "next-project-exhibition",
             script: "node_modules/next/dist/bin/next", /** 앱 실행 스크립트 */
-            args : "start -p 3000",
             autorestart : false, /** 프로세스 실패 시 자동으로 재시작 할지 여부 */
             watch: false, /** 파일이 변경 되었을 때 재시작 할지 여부  */
             env_production : {
-                NODE_ENV : "production"
+                NODE_ENV : "production",
+                PORT: 3000,
             }
         },
     ]
