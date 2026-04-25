9f2c27be593321d1653caed3e537cad379cccc1a
psh9321 <parksuhyun9321@gmail.com>
2026-04-26 03:50:33 +0900

    운영환경 실행 명령어 수정
     - prod 명령어 추가
     - ecosystem.config.js 실행 스크립트 추가
    
    pnpm start 시 ecosystem.config.js 에서 next 실행

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,14 @@
 module.exports = {
     apps : [
         {
-            name : "next-project-exhibition",
-            script: "node_modules/next/dist/bin/next", /** 앱 실행 스크립트 */
+            name : "next-project-booknote",
+            script: "pnpm", /** 앱 실행 스크립트 */
+            args : "prod",
             autorestart : false, /** 프로세스 실패 시 자동으로 재시작 할지 여부 */
             watch: false, /** 파일이 변경 되었을 때 재시작 할지 여부  */
             env_production : {
                 NODE_ENV : "production",
-                PORT: 3000,
+                PORT: 3001,
             }
         },
     ]
