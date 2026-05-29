72bf8d3fac4532e3575d6f23974517116a51bc3b
psh9321 <parksuhyun9321@gmail.com>
2026-05-29 15:58:59 +0900

    ecosystem.config.js 추가
     - config 파일 추가
     - 실행, 재실행 명령어 추가

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    apps : [
+        {
+            name : "next-project-exhibition",
+            script: "pnpm", /** 앱 실행 스크립트 */
+            args : "prod",
+            autorestart : false, /** 프로세스 실패 시 자동으로 재시작 할지 여부 */
+            watch: false, /** 파일이 변경 되었을 때 재시작 할지 여부  */
+            env_production : {
+                NODE_ENV : "production",
+                PORT: 3000,
+            }
+        },
+    ]
+}
